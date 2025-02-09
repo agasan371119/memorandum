c12e3a87a5ee8dc4fd5385783166645c6b7b5c28
agasan371119 <yna77351unichi@icloud.com>
2025-02-09 17:22:16 +0900

    “関数の復習2”

--- function.py
+++ function.py
@@ -1,7 +1,21 @@
 #関数の復習のため
 
 def say_hello():#関数の設定
-    string = input()#標準入力で読み込む
-    print(f"hello {string}")
+    # string = input()#標準入力で読み込む
+    print("hello world")
+    # print(f"hello {string}")
 
 say_hello()
+
+#関数で九九の表を作成する
+
+def kuku(x, y):
+  return x * y
+
+for num_x in range(1, 10):
+  for num_y in range(1, 10):
+    print(kuku(num_x, num_y), end="")
+    if num_y < 9:
+      print(", ", end = "")
+  
+  print("") 
