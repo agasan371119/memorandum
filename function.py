97b7f4bc4783b81f5869fc23c3e9b1c8f97d9445
agasan371119 <yna77351unichi@icloud.com>
2025-02-10 15:24:06 +0900

    “関数の復習3”

--- function.py
+++ function.py
@@ -1,21 +1,15 @@
-#関数の復習のため
-
-def say_hello():#関数の設定
-    # string = input()#標準入力で読み込む
-    print("hello world")
-    # print(f"hello {string}")
-
-say_hello()
-
-#関数で九九の表を作成する
-
-def kuku(x, y):
-  return x * y
-
-for num_x in range(1, 10):
-  for num_y in range(1, 10):
-    print(kuku(num_x, num_y), end="")
-    if num_y < 9:
-      print(", ", end = "")
+#関数定義
+def num(x):
+  return x * 2
   
-  print("") 
+print(num(3))
+
+def cnt(x):
+  return x ** 2
+
+print(cnt(3))
+
+def num_and_cnt(x):
+  return num(cnt(x))
+
+print(num_and_cnt(3))
