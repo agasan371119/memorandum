0aab4dfb2024d366e33a12654ed533501aa93a03
agasan371119 <yna77351unichi@icloud.com>
2025-02-12 20:30:43 +0900

    リスト内包表記2

--- list_com.py
+++ list_com.py
@@ -10,3 +10,18 @@ print(a)
 b = [x for x in range(10)]
 
 print(b)
+
+#条件式ありの空の繰り返し
+c = []
+
+for i in range(100):
+  if i % 13 == 0:
+    c.append(i)
+
+print(c)
+
+#リスト内包表記の条件式
+
+d = [x for x in range(10) if x % 2 == 0]
+
+print(d)
