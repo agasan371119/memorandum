1e099d14dd857e7f08e8075d681349fcb189a760
agasan371119 <yna77351unichi@icloud.com>
2025-02-17 21:12:13 +0900

    多重リストいろいろ

--- memorandum.py
+++ memorandum.py
@@ -60,3 +60,13 @@ print(b)
 for i in range(10):
   for j in range(5):
     print(i,j)
+
+#多重リスト 内包表記
+
+num = [[x for x in range(3)] for i in range(5)]
+
+print(num)
+
+for i in range(3):
+  for j in range(5):
+    print(num[i][j])
