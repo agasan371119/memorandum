7f27ad1dc91f26688d7f324b976fda9923013ad0
agasan371119 <yna77351unichi@icloud.com>
2025-03-10 19:08:09 +0900

    関数初期化

--- function.py
+++ function.py
@@ -1,30 +1,4 @@
-#関数定義
-def num(x):
-  return x * 2
-  
-print(num(3))
+def num():
+  print("Hello")
 
-def cnt(x):
-  return x ** 2
-
-print(cnt(3))
-
-def num_and_cnt(x, y):
-  return num(cnt(x、y))
-
-print(num_and_cnt(3,2))
-
-def num1_num2(a, b):
-  return a + b
-
-print(num1_num2(5, 6))
-
-def loop():
-  print("good")
-
-loop()
-
-def sum(a, b):
-  return a // b
-
-print(sum(10, 20))
+num()  
