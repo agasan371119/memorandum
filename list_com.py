ed7ec7ee0adb28876f4903da7a6aa971bddb353f
agasan371119 <yna77351unichi@icloud.com>
2025-03-15 16:35:20 +0900

    辞書格納練習6

--- list_com.py
+++ list_com.py
@@ -28,14 +28,14 @@ print(dict)
 
 #辞書
 #空の辞書
-dict1 = {}
+dict_1 = {}
 
 #繰り返し格納
 for i in range(5):
   a, b = input().split()
-  dict1[a] = int(b)
+  dict_1[a] = int(b)
   
-print(dict1)
+print(dict_1)
 
 #辞書の内包表記
 dict_1 = [input().split() for x in range(5)]
